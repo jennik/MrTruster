38ab3feb1995adcbff1b468ab499ffa3a957febb
jennik <evg@jennik.ru>
2014-03-01 14:43:04 +0400

    Initial Commit

---
+++ Mr. Truster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mr. Truster
+//
+//  Created by evg on 01.03.14.
+//  Copyright (c) 2014 redbrush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
---
+++ Mr. TrusterTests/Mr__TrusterTests.m
@@ -0,0 +1,34 @@
+//
+//  Mr__TrusterTests.m
+//  Mr. TrusterTests
+//
+//  Created by evg on 01.03.14.
+//  Copyright (c) 2014 redbrush. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mr__TrusterTests : XCTestCase
+
+@end
+
+@implementation Mr__TrusterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
