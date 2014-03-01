e86d2baac598e7cf7c4f8ff0ec813f5772edee13
excpt <ilya.grinzovskiy@gmail.com>
2014-03-01 18:12:25 +0400

    Добавил тестовые фотки и 5 тестовых элементов при инициализации вьюхи.

--- Mr. Truster/MTMainViewController.m
+++ Mr. Truster/MTMainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MTMainViewController.h"
+#import "MTRecord.h"
 
 @interface MTMainViewController ()
 
@@ -21,6 +22,31 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+
+        NSDate *date = [NSDate date];
+        ABAddressBookRef addressBook = ABAddressBookCreate();
+        CFArrayRef allPeople = ABAddressBookCopyArrayOfAllPeople(addressBook);
+        ABRecordRef ref = CFArrayGetValueAtIndex(allPeople, 1);
+        
+        UIImage *item_img1 = [UIImage imageNamed:@"item_img1"];
+        UIImage *item_img2 = [UIImage imageNamed:@"item_img2"];
+        UIImage *item_img3 = [UIImage imageNamed:@"item_img3"];
+        UIImage *item_img4 = [UIImage imageNamed:@"item_img4"];
+        UIImage *item_img5 = [UIImage imageNamed:@"item_img5"];
+        
+        UIImage *deb_img1 = [UIImage imageNamed:@"deb_img1"];
+        UIImage *deb_img2 = [UIImage imageNamed:@"deb_img2"];
+        UIImage *deb_img3 = [UIImage imageNamed:@"deb_img3"];
+        UIImage *deb_img4 = [UIImage imageNamed:@"deb_img4"];
+        UIImage *deb_img5 = [UIImage imageNamed:@"deb_img5"];
+        
+        MTRecord *item1 = [[MTRecord alloc] initWithDateBack:date debitorPhoto:deb_img1 itemPhoto:item_img1 comment:@"Plz giv ma stuff" contact:ref];
+        MTRecord *item2 = [[MTRecord alloc] initWithDateBack:date debitorPhoto:deb_img2 itemPhoto:item_img2 comment:@"Plz giv ma stuff" contact:ref];
+        MTRecord *item3 = [[MTRecord alloc] initWithDateBack:date debitorPhoto:deb_img3 itemPhoto:item_img3 comment:@"Plz giv ma stuff" contact:ref];
+        MTRecord *item4 = [[MTRecord alloc] initWithDateBack:date debitorPhoto:deb_img4 itemPhoto:item_img4 comment:@"Plz giv ma stuff" contact:ref];
+        MTRecord *item5 = [[MTRecord alloc] initWithDateBack:date debitorPhoto:deb_img5 itemPhoto:item_img5 comment:@"Plz giv ma stuff" contact:ref];
+        
+        records = [NSMutableArray arrayWithObjects:item1, item2, item3, item4, item5, nil];
     }
     return self;
 }
@@ -41,7 +67,5 @@
 {
     return [self.records count];
 }
-
-- 
 
 @end
